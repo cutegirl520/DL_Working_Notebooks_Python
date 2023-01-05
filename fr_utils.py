39ceb6310425866abe298cbeb21f259b27b80dc9
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-01-05 11:16:31 +0000

    * bugfix

---
+++ fr_utils.py
@@ -0,0 +1,8 @@
+#### PART OF THIS CODE IS USING CODE FROM VICTOR SY WANG: https://github.com/iwantooxxoox/Keras-OpenFace/blob/master/utils.py ####
+
+import tensorflow as tf
+import numpy as np
+import os
+import cv2
+from numpy import genfromtxt
+from ker
