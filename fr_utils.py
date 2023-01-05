150d2ad600ed8cd7edbc51d913a0bf29b4bf262d
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-01-05 18:56:54 +0000

    doxygen fixes

--- fr_utils.py
+++ fr_utils.py
@@ -165,4 +165,33 @@ def load_weights():
         elif 'dense' in name:
             dense_w = genfromtxt(dirPath+'/dense_w.csv', delimiter=',', dtype=None)
             dense_w = np.reshape(dense_w, (128, 736))
-            dense_w = np.transpose
+            dense_w = np.transpose(dense_w, (1, 0))
+            dense_b = genfromtxt(dirPath+'/dense_b.csv', delimiter=',', dtype=None)
+            weights_dict[name] = [dense_w, dense_b]
+
+    return weights_dict
+
+
+def load_dataset():
+    train_dataset = h5py.File('datasets/train_happy.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_happy.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+def img_to_encoding(image_path, model):
+    img1 = cv2.imread(image_path, 1)
+    img = img1[...,::-1]
+    img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
+    x_train = np.array([img])
+    embedding = model.predict_on_batch(x_train)
+    return embedding
