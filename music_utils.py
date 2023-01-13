ca55f83c89d3d1c466d21bc4b23a123917215efe
cutegirl520 <160213624+cutegirl520@users.noreply.github.com>
2023-01-13 09:51:15 +0000

    Run update-remote.sh on erlang

--- music_utils.py
+++ music_utils.py
@@ -45,4 +45,48 @@ def next_value_processing(model, next_value, x, predict_and_sample, indices_valu
     max_tries -- Maximum numbers of time trying to fix the value
     
     Returns:
-    next_value 
+    next_value -- process predicted value
+    """
+
+    # fix first note: must not have < > and not be a rest
+    if (duration < 0.00001):
+        tries = 0
+        while (next_value.split(',')[0] == 'R' or 
+            len(next_value.split(',')) != 2):
+            # give up after 1000 tries; random from input's first notes
+            if tries >= max_tries:
+                #print('Gave up on first note generation after', max_tries, 'tries')
+                # np.random is exclusive to high
+                rand = np.random.randint(0, len(abstract_grammars))
+                next_value = abstract_grammars[rand].split(' ')[0]
+            else:
+                next_value = predict_and_sample(model, x, indices_values, temperature)
+
+            tries += 1
+            
+    return next_value
+
+
+def sequence_to_matrix(sequence, values_indices):
+    """
+    Convert a sequence (slice of the corpus) into a matrix (numpy) of one-hot vectors corresponding 
+    to indices in values_indices
+    
+    Arguments:
+    sequence -- python list
+    
+    Returns:
+    x -- numpy-array of one-hot vectors 
+    """
+    sequence_len = len(sequence)
+    x = np.zeros((1, sequence_len, len(values_indices)))
+    for t, value in enumerate(sequence):
+        if (not value in values_indices): print(value)
+        x[0, t, values_indices[value]] = 1.
+    return x
+
+def one_hot(x):
+    x = K.argmax(x)
+    x = tf.one_hot(x, 78) 
+    x = RepeatVector(1)(x)
+    return x
